f4f40473ed93712c4c17895f990289aa79798eb7
aparna <aparna.ur@experionglobal.com>
2019-11-15 16:33:30 +0530

    github profile viewer app using angular material component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+  isLoaded: boolean;
+  isFound: boolean;
+  ifUserAvailable: boolean;
   title = 'githubProfileViewer';
+  userdata: any;
+  name: any;
+  avatarUrl: any;
+  location: any;
+  htmlUrl: any;
+  localdata: any;
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      username: new FormControl('')
+    });
+    this.ifUserAvailable = false;
+  }
+    findUser() {
+      this.ifUserAvailable = true;
+      if (localStorage.getItem(this.myForm.value.username)) {
+        this.localdata = JSON.parse(localStorage.getItem(this.myForm.value.username));
+        this.display(this.localdata);
+      } else {
+        this.isLoaded = true;
+        // tslint:disable-next-line: max-line-length
+        this.http.get('https://api.github.com/users/' + this.myForm.value.username + '?access_token=beba3c150021bfb49769385927dfa59fac2cdf04').subscribe(data => {
+        this.localdata = data;
+        this.display(this.localdata);
+        this.isLoaded = false;
+        localStorage.setItem(this.myForm.value.username, JSON.stringify(this.localdata));
+      }, error => {
+        this.isFound = false;
+        this.openSnackBar('No results found', '');
+      });
+      }
+    }
+    display(localdata) {
+      this.name = localdata.name;
+      this.avatarUrl = localdata.avatar_url;
+      this.location = localdata.location;
+      this.htmlUrl = localdata.html_url;
+      }
+      openSnackBar(message: string, action: string) {
+        this.ifUserAvailable = false;
+        this.isLoaded = false;
+        this.snackBar.open(message, action, {
+          duration: 4000,
+        });
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
